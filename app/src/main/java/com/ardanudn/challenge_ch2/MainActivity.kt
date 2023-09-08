808c0f396c5fc343680ed3e85f1ac5fb3c1e8f00
Ardanudn <danuduhri28@gmail.com>
2023-09-08 19:51:48 +0700

    finish layout with constraint and recyclerview

--- app/src/main/java/com/ardanudn/challenge_ch2/MainActivity.kt
+++ app/src/main/java/com/ardanudn/challenge_ch2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.ardanudn.challenge_ch2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.rv_menu)
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+
+        // Inisialisasi data menu makanan
+        val menuMakanan = mutableListOf<MenuMakanan>()
+        menuMakanan.add(MenuMakanan(R.drawable.burger, "Burger", "Rp. 28.000"))
+        menuMakanan.add(MenuMakanan(R.drawable.ayamgoreng, "Ayam Goreng", "Rp. 20.000"))
+        menuMakanan.add(MenuMakanan(R.drawable.ayampanggang, "Ayam Panggang", "Rp. 25.000"))
+        menuMakanan.add(MenuMakanan(R.drawable.dimsum, "Dim Sum", "Rp. 27.000"))
+
+        // Inisialisasi adapter
+        val menuAdapter : MenuAdapter = MenuAdapter(menuMakanan)
+        recyclerView.adapter = menuAdapter
     }
 }
